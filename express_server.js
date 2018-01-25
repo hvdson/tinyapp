922fde2e032824365627baf533bd46baf84cd9fa
hvdson <hvdsonmusic@gmail.com>
2018-01-25 23:41:05 +0000

    Finished user registration

--- express_server.js
+++ express_server.js
@@ -25,7 +25,15 @@ const users = {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "dishwasher-funk"
+  },
+  
+  "test": {
+    id: "test",
+    email: "test@test.com",
+    password: "test"
   }
+
+
 };
 
 function registerFieldsEmpty(emailAddress, password) {
@@ -34,6 +42,22 @@ function registerFieldsEmpty(emailAddress, password) {
 
 function generateRandomString() {
   return Math.random().toString(36).replace(/[^a-zA-Z0-9]+/g, '').substr(0, 6);
+}
+
+function passwordConfirmation(password, passwordConfirm) {
+  return password === passwordConfirm;
+}
+
+// return user obj if email matches
+// return false if doesn't exist
+function databaseHasUserEmail(email) {
+  // users is a global var
+  for (let user in users) {
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return false;
 }
 
 function getUserByEmail(email) {
@@ -46,21 +70,33 @@ function getUserByEmail(email) {
   return false;
 }
 
-// Middleware
+// MIDDLEWARE
+// ---------------------------------------------
+// allows cookies to be stored in locals
+// don't need templateVars for cookies now b/c user cookies are
+// stored in the local obj of res(ponse)
+// express.ejs handles this
 
 app.use(function(req, res, next) {
+  // everytime server receives a request first pass through middleware
   res.locals.user = users[req.cookies.user_id];
+  // POST that handles register && login will set the cookie
+  // on first access of page w.o cookie will print { user: undefined }
   console.log(res.locals);
   next();
 });
-// GET request
+
+
+// GET requests:
+// should only res(pond) with readable data - NOT changing anything
 // ---------------------------------------------
+
 app.get("/", (req, res) => {
-  // console.log("username: ", req);
   let templateVars = {
     urls: urlDatabase,
     user: req.cookies["user_id"]
   };
+  // express only looks for FILES the views directory
   res.render("pages/urls_index", templateVars);
 });
 
@@ -71,10 +107,14 @@ app.get("/urls", (req, res) => {
   res.render("pages/urls_index", templateVars);
 });
 
+// app.get("/register", (req, res) => {
+//   res.render("pages/register", {
+//     user: users[req.cookies.user_id]
+//   });
+// });
+
 app.get("/register", (req, res) => {
-  res.render("pages/register", {
-    user: users[req.cookies.user_id]
-  });
+  res.render("pages/register");
 });
 
 app.get("/urls/new", (req, res) => {
@@ -84,11 +124,15 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortURL: req.params.id,
-    longURL: urlDatabase,
-    user: req.cookies["user_id"]
+    longURL: urlDatabase
   };
   res.render("pages/urls_show", templateVars);
 });
+
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+});
+
 
 // POST requests
 // ---------------------------------------------
@@ -101,7 +145,6 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-
   if (req.body.shortURL.length > 0) {
     urlDatabase[req.params.id] = req.body.shortURL;
   }
@@ -116,63 +159,63 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
-  const email = req.body.email;
+  
+  // get the email, password & conf. from user input using deconstruction
+  // request.body needs to set NAME as a key
+  const obj = { email, password, confirmPassword } = req.body;
+  console.log(obj.email, obj.password, obj.confirmPassword);
+  // returns user if email is in database - false otherwise
   const user = getUserByEmail(email);
-
+  console.log(user);
+  // if the user exists from email lookup
   if (user) {
-    res.cookie("user_id", user.id);
-    res.redirect(`/urls`);
-  } else {
-    res.status(404).redirect('/register');
-  }
+    // check if the password (and confirmPass) match database
+    if (obj.password === user.password && obj.confirmPassword === user.password) {
 
+      // work normally
+      res.locals.user = user;
+      res.cookie("user_id", user.id);
+      // res.locals.user = {};
+      
+      res.redirect("/urls");
+      // password field not correct - redirect
+    } else {
+      console.log("Passwords don't match.");
+      res.status(403).redirect('/login');
+
+    }
+  } else {
+    console.log("User doesn't exist");
+    // alert("User not found - try again ");
+    res.status(404).redirect('/login');
+  }
 });
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
+  // res.local.user = {};
   res.redirect(`/urls`);
   // res.send();
 });
 
 app.post("/register", (req, res) => {
-  // console.log("[---------- request ----------]");
-  // console.log(res);
-  // console.log("[---------- response ----------]");
-
-  // const newUserId = generateRandomString();
-  // const emailAddress = req.body.emailAddress;
-  // const password = req.body.password;
-
-  // if (registerFieldsEmpty(emailAddress, password)) {
-  //   res.status(400).render("pages/400");
-  // }
-  
-  // if (!users.hasOwnProperty(newUserId)) {
-  //   users[newUserId] = {};
-  // }
-  
-  // users[newUserId]["user_id"] = newUserId;
-  // users[newUserId]["email"] = req.body.emailAddress;
-  // users[newUserId]["password"] = req.body.password;
-
-  // res.cookie("user_id", newUserId);
-
-  // let templateVars = {
-    // urls: urlDatabase
-  // };
-  // console.log(req.cookies.user_id);
-
-  // res.render(`pages/urls_index`, templateVars);
 
   const id = generateRandomString();
-  const { email, password } = req.body;
+  const { email, password, confirmPassword } = req.body;
 
   if (registerFieldsEmpty(email, password)) {
     res.status(400).render("pages/400");
-  } 
+  } else if (confirmPassword !== password) {
+    console.log("Password confirmation does not match!");
+    res.status(400).redirect("/register");
+  } else if (getUserByEmail(email)) {
+    console.log("User already exists!");
+    // https://stackoverflow.com/questions/9269040/which-http-response-code-for-this-email-is-already-registered
+    res.status(409).redirect("/register");
+  }
 
   users[id] = {
+    id: id,
     email: email,
     password: password
   };
@@ -180,9 +223,6 @@ app.post("/register", (req, res) => {
   res.cookie("user_id", id);
 
   res.redirect(`/urls`);
-  // QUESTION asky why below prints undefined in console
-  // console.log(res.cookie.username);
-
 });
 
 app.listen(PORT, () => {
